22ed56ef39e3e11ea2992c1249e7d4b409aa1128
Niko Storni <niko@odysee.com>
2022-10-10 16:38:43 +0200

    spoof user agent

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -9,7 +9,17 @@ import (
 )
 
 func DownloadFile(URL string, isRetry bool) ([]byte, error) {
-	response, err := http.Get(URL)
+	method := "GET"
+
+	client := &http.Client{
+		Timeout: time.Second * 20,
+	}
+	req, err := http.NewRequest(method, URL, nil)
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+	response, err := client.Do(req)
 	if err != nil {
 		return nil, errors.Err(err)
 	}
